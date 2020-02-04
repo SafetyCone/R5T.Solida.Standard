1ff57aaf9bee84eb001ab6f65f1b3c0243dffeed
MinexAutomation <minex.automation@gmail.com>
2020-02-03 23:04:09 -0800

    Added IServiceCollection extensions.

--- source/R5T.Solida.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Solida.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Solida.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Solida.Solutas;
+using R5T.Solutas.Standard;
+
+
+namespace R5T.Solida.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IVisualStudioSolutionFileSiteSerializer"/> service.
+        /// </summary>
+        public static IServiceCollection AddVisualStudioSolutionFileSiteSerializer(this IServiceCollection services)
+        {
+            services.AddVisualStudioSolutionFileSiteSerializer(services.AddVisualStudioSolutionFileSerializerAction());
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IVisualStudioSolutionFileSiteSerializer"/> service.
+        /// </summary>
+        public static ServiceAction<IVisualStudioSolutionFileSiteSerializer> AddVisualStudioSolutionFileSiteSerializerAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioSolutionFileSiteSerializer>(() => services.AddVisualStudioSolutionFileSiteSerializer());
+
+            return serviceAction;
+        }
+    }
+}
